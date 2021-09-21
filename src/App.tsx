a2d1204d4dcffee964c69015fe2906c99f3e7e26
Juliana <aragaopintojuli@gmail.com>
2021-09-21 14:25:56 -0300

    page scope

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [lampada, setlampada] = useState(false);
+  const [ar, setar] = useState(false);
+  const [alarme, setalarme] = useState(false);
+
+  const toggleConvert = (value: boolean) => {
+    return value ? 'Ligado' : 'Desligado';
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>MI Sistemas Digitais</p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <p>Lâmpadas: {toggleConvert(lampada)}</p>
+        <button onClick={() => setlampada(!lampada)}>Toggle</button>
+
+        <p>Ar condicionado: {toggleConvert(ar)}</p>
+        <button onClick={() => setar(!ar)}>Toggle</button>
+
+        <p>Alarme: {toggleConvert(alarme)}</p>
+        <button onClick={() => setalarme(!alarme)}>Toggle</button>
       </header>
     </div>
   );
